faec1e5db393dd81798db4102dd1d42b815204e1
Melike Kaya <112420252+melikkekaya@users.noreply.github.com>
2023-05-10 10:27:44 +0200

    doc strings

--- utils/game.py
+++ utils/game.py
@@ -5,7 +5,7 @@ class Hangman():
         self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
         self.word_to_find = [letter for letter in random.choice(self.possible_words)]
 
-        self.correctly_guessed_letters = ["_" for letter in self.word_to_find]
+        self.correctly_guessed_letters = ["_" for letter in self.word_to_find]  # add '_' for each letter of the word_to_find into a list
         self.wrongly_guessed_letters = []
 
         self.lives = 5
@@ -13,13 +13,16 @@ class Hangman():
         self.error_count = 0
 
     def play(self):
+        """ 
+        This method asks the player to enter a letter and checks whether this letter is included in the 'word_to_find'. 
+        If so, it changes with the '_' character with the letter.
+        """
         self.letter = input("Please enter a letter: ").lower()
-        if self.letter.isalpha() and len(self.letter) == 1:
-
-            if self.letter in self.word_to_find:
+        if self.letter.isalpha() and len(self.letter) == 1:   # checks letter is not a non-alphabetical character and not more than one letter.
+            if self.letter in self.word_to_find:                
                 if self.letter in self.correctly_guessed_letters:
                     print("You've already found this letter..")
-                else:
+                else:                                         # to find indexes of the letter in word_to_find and change all _ with the letter
                     matched_indexes = []
                     i = 0
                     length = len(self.word_to_find)
@@ -48,6 +51,12 @@ class Hangman():
             print("You've entered an unexceptable character..")
 
     def start_game(self):
+        """
+        This method calls;
+        - 'play()' until the game is won finding the word correctly or the game is over.
+        - 'well_played()' if the word is found before lives are out.
+        - 'game_over()' if 5 lives are out.
+        """
         while self.lives > 0:
             self.play()
             if '_' not in self.correctly_guessed_letters:
@@ -58,10 +67,16 @@ class Hangman():
                 break
 
     def game_over(self):
+        """
+        This method stops the game when whole 5 lives are out and prints that the game is over.
+        """
         word = "".join(self.word_to_find).upper()
         print(f"You're out of lives! Game is over.. The word was {word}")
 
     def well_played(self):
+        """
+        This method stops the game when the user find the word correctly and prints word_to_find, turn_count, and error_count.
+        """
         word = "".join(self.word_to_find).upper()
         print(f"You found the word: {word} in {self.turn_count} turns with {self.error_count} errors!")
         
